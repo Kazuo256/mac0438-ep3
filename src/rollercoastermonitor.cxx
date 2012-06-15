729146e06304b33f8e29f5d64989738e977e4ab1
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-06-15 20:09:36 -0300

    Save state

--- src/rollercoastermonitor.cxx
+++ src/rollercoastermonitor.cxx
@@ -1,20 +1,47 @@
 
 #include "rollercoastermonitor.h"
 
+#include "car.h"
 #include "mutex.h"
 #include "log.h"
 
 namespace ep3 {
 
 void RollerCoasterMonitor::pegaCarona (unsigned psg_id, bool golden) {
+  Mutex::Lock lock(mutex_);
+  // Wait for an available car.
+  wait(available_car_, golden ? 0 : 1);
+  // Enter the car and wait for the lap to end.
+  signal(psg_aboard_);
+  wait(lap_end_);
 }
 
-void RollerCoasterMonitor::carrega () {
-
+void RollerCoasterMonitor::carrega (unsigned car_id) {
+  Mutex::Lock lock(mutex_);
+  // Make sure to be the next car to load.
+  // TODO
+  // Wait for enough passengers to arrive.
+  // TODO
+  // Wait for passengers to fully board the car.
+  for (unsigned i = 0; i < cap; i++) {
+    signal(available_car_);
+    wait(psg_aboard_);
+  }
+  cars_riding_.push(car_id);
+  Log().debug("Car #"+utos(car_id)+" has started a new lap.");
 }
 
-void RollerCoasterMonitor::descarrega () {
-
+void RollerCoasterMonitor::descarrega (unsigned car_id) {
+  Mutex::Lock lock(mutex_);
+  // Make sure the car wasn't outrunned.
+  while (cars_riding_.front() != car_id)
+    wait(riding_order_);
+  cars_riding_.pop();
+  Log().debug("Car #"+utos(car_id)+" has finished its lap.");
+  signal_all(riding_order_);
+  // Let the passengers leave.
+  for (unsigned i = 0; i < cap; i++)
+    signal(lap_end_);
 }
 
 void RollerCoasterMonitor::start_lap (unsigned car_id) {
@@ -32,15 +59,15 @@ void RollerCoasterMonitor::finish_lap (unsigned car_id) {
   signal_all(riding_order_);
 }
 
-void RollerCoasterMonitor::testA (Rank rank) {
-  Mutex::Lock lock(mutex_);
-  wait(testcv_, rank);
-}
-
-void RollerCoasterMonitor::testB () {
-  Mutex::Lock lock(mutex_);
-  signal(testcv_);
-}
+//void RollerCoasterMonitor::testA (Rank rank) {
+//  Mutex::Lock lock(mutex_);
+//  wait(testcv_, rank);
+//}
+//
+//void RollerCoasterMonitor::testB () {
+//  Mutex::Lock lock(mutex_);
+//  signal(testcv_);
+//}
 
 } // namespace ep3
 
--- src/rollercoastermonitor.h
+++ src/rollercoastermonitor.h
@@ -9,22 +9,38 @@
 
 namespace ep3 {
 
+class Car;
+
 class RollerCoasterMonitor : public Monitor {
   public:
     RollerCoasterMonitor () :
       Monitor(),
-      testcv_(2) {}
+      //testcv_(2),
+      available_car_(2),
+      waiting_psgs_count_(0),
+      next_car_(NULL) {}
     void pegaCarona (unsigned psg_id, bool golden);
-    void carrega ();
+    void carrega (std::vector<unsigned>& car_sits);
     void descarrega ();
     //
     void start_lap (unsigned car_id);
     void finish_lap (unsigned car_id);
     //
-    void testA (Rank rank);
-    void testB ();
+    //void testA (Rank rank);
+    //void testB ();
   private:
-    CondVar               testcv_;
+    //CondVar               testcv_;
+    // Used as a queue for the incoming passengers.
+    CondVar               available_car_;
+    // Used as a queue for the arriving cars.
+    CondVar               full_cap_;
+    unsigned              waiting_psgs_count_;
+    // Used to know when the car loading order has been updated.
+    CondVar               load_order_;
+    // Used to know the order in which the cars start to load.
+    std::vector<unsigned> psg_buffer_;
+    Car                   *next_car_;
+    ////////
     // Used to know when the car riding order has been updated.
     CondVar               riding_order_;
     // Used to know the order in which the cars enter the rails.
